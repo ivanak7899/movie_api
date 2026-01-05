f8d2357b704dd4340172c9ef736ea90caa5f5d9f
Ivana <ivankris.math@pmf.hr>
2026-01-05 13:20:17 +0100

    define only needed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :castings
-  resources :movies
-  resources :people
-  resources :users
-
+  resources :reviews, only: [:index, :show, :create, :update, :destroy]
+  resources :castings, only: [:index, :show, :create, :update, :destroy]
+  resources :movies, only: [:index, :show, :create, :update, :destroy]
+  resources :people, only: [:index, :show, :create, :update, :destroy]
+  resources :users, only: [:index, :show, :create, :update, :destroy]
   resource :session, only: [ :create, :destroy ]
 end

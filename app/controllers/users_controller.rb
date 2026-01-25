5c49e036ca91417e32df0bbe86031240e39ebae3
Ivana <ivankris.math@pmf.hr>
2026-01-25 17:13:28 +0100

    forbid setting user role in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :role)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
